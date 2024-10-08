6e00c1863bfe7ffda96116e148b4438c899edc7b
Thomas Dufour <chwthewke@gmail.com>
2024-10-08 07:12:33 +0200

    feat: all tiers in recipe view, totals in bill view

--- web-v2/src/main/scala/net/chwthewke/satisfactorytools/web/view/BillView.scala
+++ web-v2/src/main/scala/net/chwthewke/satisfactorytools/web/view/BillView.scala
@@ -1,36 +1,56 @@
 package net.chwthewke.satisfactorytools
 package web.view
 
+import cats.syntax.foldable._
 import cats.syntax.show._
 import scalatags.Text
 import scalatags.Text.Tag
 
 import data.Countable
+import data.Form
 import data.Item
 import model.Bill
 import model.Model
+import model.Recipe
 import web.forms
 
 object BillView extends ( ( Model, Bill ) => Tag ) {
   import Text.all._
 
   def apply( model: Model, bill: Bill ): Tag = {
-    val eligibleRecipes =
+    val eligibleRecipes: Vector[Recipe] =
       model.manufacturingRecipes
 
-    val eligibleItems =
+    val eligibleItems: Vector[Item] =
       model.items.values
         .filter( item => eligibleRecipes.exists( recipe => recipe.products.exists( _.item == item ) ) )
         .toVector
+
+    val items: Vector[Countable[Double, Item]] =
+      eligibleItems.sorted.map( item => bill.items.find( _.item == item ).getOrElse( Countable( item, 0d ) ) )
+
+    val ( ( count, total ), ( countE, totalE ) ) =
+      items
+        .filter( _.amount > AmountTolerance )
+        .foldMap { it =>
+          val counted: ( Int, Double ) = ( 1, it.amount )
+
+          if (
+            it.item.className.name.startsWith( "Desc_SpaceElevatorPart_" ) ||
+            it.item.form != Form.Solid
+          ) ( counted, ( 0, 0d ) )
+          else ( counted, counted )
+        }
 
     fieldset(
       legend( "Production target (per minute)" ),
       table(
         thead( tr( th( "Item" ), th( "Amount" ) ) ),
-        tbody(
-          eligibleItems.sorted
-            .map( item => bill.items.find( _.item == item ).getOrElse( Countable( item, 0d ) ) )
-            .map( itemField )
+        tbody( items.map( itemField ) ),
+        tfoot(
+          tr( td( colspan := "2", s"$count items requested, $total/min total." ) ),
+          tr( td( colspan := "2", "Excluding fluids and project parts:" ) ),
+          tr( td( colspan := "2", s"$countE items requested, $totalE/min total." ) )
         )
       )
     )
